9404b6777ef87d5221a57367d0469ae540955b21
Aidan Monty <amonty@wisc.edu>
2021-10-28 23:33:47 -0500

    set up basic welcome screen

---
+++ app/src/main/java/com/example/helpmefind/Welcome.java
@@ -0,0 +1,16 @@
+package com.example.helpmefind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+    }
+}
