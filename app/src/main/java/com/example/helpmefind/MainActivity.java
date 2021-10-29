9f7a2529ce99e4528635a4a286292c8366646d89
Aidan Monty <amonty@wisc.edu>
2021-10-29 00:21:24 -0500

    linked several activities and tried (unsuccessfully) to get progress bar to work

--- app/src/main/java/com/example/helpmefind/MainActivity.java
+++ app/src/main/java/com/example/helpmefind/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helpmefind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Right now MainActivity doesn't do anything and goes straight
+        // to Welcome
+        Intent intent = new Intent(this, Welcome.class);
+        startActivity(intent);
     }
 }
