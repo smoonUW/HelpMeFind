670fa3f0ac322e8f42e948982b1e90f61496c6b5
Tyler J. Johnston <tylerj.johnston@outlook.com>
2021-10-31 19:21:52 -0500

    MainActivity no longer launches MapView.

--- app/src/main/java/com/example/helpmefind/MainActivity.java
+++ app/src/main/java/com/example/helpmefind/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_view);
+        setContentView(R.layout.activity_main);
     }
 }
