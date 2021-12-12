6a2bf3cc5902e1d200040b453fa93558f16133ed
Tyler J. Johnston <tylerj.johnston@outlook.com>
2021-12-12 11:22:19 -0600

    All styling largely completed (other than the Wayfinder page. That could use some work)

--- app/src/main/java/com/example/helpmefind/Welcome.java
+++ app/src/main/java/com/example/helpmefind/Welcome.java
@@ -9,29 +9,14 @@ import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 public class Welcome extends AppCompatActivity implements View.OnClickListener {
-
-    // activity does not show, issue may be with thread.sleep,
-    // either main thread refuses to sleep or another method
-    // is needed (onPostDelayed?) instead of thread.sleep. in
-    // either case the progress bar is just for
-    // show at the moment
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
         this.findViewById(R.id.welcomeTitle).setOnClickListener(this::onClick);
         this.findViewById(R.id.welcomeSubheader).setOnClickListener(this::onClick);
         this.findViewById(R.id.imageView2).setOnClickListener(this::onClick);
-//        ProgressBar progressBar=(ProgressBar) findViewById(R.id.progressBar);
-//        for (int i = 1; i <= 100; i++) {
-//            progressBar.setProgress(i);
-//            try {
-//                Thread.sleep(50);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
     }
 
     /**
